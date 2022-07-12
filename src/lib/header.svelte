8e9106312614048de41fe2293809792e3fe39ae7
gabidev <gapelidev@gmail.com>
2022-07-12 16:47:54 +0200

    feat: menu escritorio

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,97 +1,42 @@
 <script>
-import Modal from "./modal.svelte";
 import RedesSociales from "./redes-sociales.svelte";
+import Menu from "$lib/menu.svelte";
 
 let showModal = false;
 let nombre = "Gabi Pérez";
 
 </script>
-<header>
-  <div>
+<header class="global-container">
+  <div class="header-container">
+   <div class="header-logo">
     <a href="/">
       <h1>{nombre}</h1>
     </a>
+   </div>
+   <div class="header-curriculum">
+    <button>Descargar CV</button>
+   </div>
   </div>
-  <nav>
-    <ul>
-      <li class="header-link">
-        <a href="/">Home</a>
-      </li>
-      <li class="header-link">
-        <a href="/conoceme">Conóceme</a>
-      </li>
-      <li class="header-link">
-        <a href="/portfolio">Portfolio</a>
-      </li>
-      <li class="header-link">
-        <a href="/proyectos">Proyectos</a>
-      </li>
-      <li>
-        <button on:click="{() => showModal = true}">
-          ¿Hablamos?
-        </button>
-      </li>
-    </ul>
-  </nav>
 
-  {#if showModal}
-	<Modal on:close="{() => showModal = false}">
-		
-    <section class="section-modal-container">
-      <div class="header-modal">
-        <h2>{nombre}</h2>
-      </div>
-      <div class="text-modal">
-        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and will give you a complete account of the system and expound the actual teachings of the great explore</p>
-      </div>
-      <div class="info-modal">
-        <h4>Información de Contacto</h4>
-        <div class="info-modal-container">
-          <a href="tel:601068778"><i class="fa-solid fa-phone"></i><span>601068778</span></a>
-          <a target="_blank" href="mailto:gapedev@gmail.com"><i class="fa-solid fa-envelope"></i><span>gapedev@gmail.com</span></a>
-        </div>
-      </div>
-    </section>
+  <Menu/>
 
-    <section>
-      <RedesSociales/>
-    </section>
+  <RedesSociales/>
 
-	</Modal>
-{/if}
 </header>
 
 <style>
-  header {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    padding: 1.4375rem 0 0 0;
-    background-image: radial-gradient(circle at 29.9% 42.68%, #51bef9 0, #3cb5f2 50%, #1daceb 100%);
-  }
+ .header-container {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 20px 0;
+ }
 
   h1 {
     font-size: 1.5rem;
     font-weight: 500;
   }
 
-  ul {
-    display: flex;
-    align-items: center;
-  }
-
-  li {
-    list-style: none;
-    margin-right: 1.25rem;
-    font-weight: bold;
-    transition: 0.3s;
-    letter-spacing: 0.0688rem;
-  }
-
-  li:hover {
-    color: #0052CC;
-    transition: 0.3s;
-  }
   button {
   background-color:#0052CC;
   border: none;
@@ -100,48 +45,5 @@ let nombre = "Gabi Pérez";
   cursor: pointer;
   padding:0.625rem;
   border-radius: 0.625rem;
-}
-
-.text-modal, .info-modal {
-  padding: 1.25rem 0;
-}
-
-.info-modal h4 {
-  font-weight: 500;
-  font-size: 1.25rem;
-}
-
-.info-modal-container {
-  display: flex;
-  flex-direction: column;
-  margin-top: 1rem;
-}
-
-.info-modal-container a {
-  margin: 0.3125rem 0;
-}
-
-.info-modal-container span{
-  font-size: 1.125rem;
-  font-weight: 600;
-  transition: ease-in-out 0.3s;
-}
-
-.info-modal-container span:hover{
-  color: #0052CC;
-  transition: ease-in-out 0.3s;
-}
-
-.info-modal-container i{
-    width: 2.5rem;
-    height: 2.5rem;
-    line-height: 40px;
-    text-align: center;
-    border-radius: 50%;
-    margin-right: 0.625rem;
-    box-shadow: 0rem 0.625rem 0.9375rem 0rem rgb(0 0 0 / 10%);
-    font-size: 1rem;
-    flex: 0 0 auto;
-    color: #0052CC;
 }
 </style>
---
+++ src/lib/menu.svelte
@@ -0,0 +1,180 @@
+
+
+<nav>
+
+   <div class="main-menu">
+    <ul>
+      <li class="">
+        <a href="/">
+        <div>
+          <i class="fa-solid fa-house-chimney"></i>
+        </div>
+        <span>Inicio</span>
+        </a>
+      </li>
+      <li class="active">
+        <a href="/conoceme"> 
+        <div>
+          <i class="fa-solid fa-address-card"></i>
+        </div>
+        <span>Conóceme</span>
+      </a>
+      </li>
+      <li class="active">
+        <a href="/portfolio">
+          <div>
+          <i class="fa-solid fa-tarp"></i>
+        </div>
+        <span>Portfolio</span>
+      </a>
+      </li>
+      <li class="active">
+        <a href="/proyectos">
+          <div>
+          <i class="fa-solid fa-diagram-project"></i>
+        </div>
+        <span>Proyectos</span>
+      </a>
+      </li>
+      <li class="active">
+        <a href="/contacto">
+          <div>
+            <i class="fa-solid fa-envelope"></i>
+          </div>
+          <span>Contacto</span>
+        </a>
+      </li>
+    </ul>
+   </div>
+  
+</nav> 
+
+<style>
+
+  .main-menu {
+    position: fixed;
+    top: 50%;
+    -webkit-transform: translateY(-50%);
+    transform: translateY(-50%);
+    width: 100%;
+    right: 0;
+    padding: 0 2vw;
+    max-width: 15%
+  }
+  
+  .main-menu ul li {
+    margin: 0 0 12px;
+    background-color: #fff;
+    position: relative;
+    height: 70px;
+    width: 70px;
+    padding: 23px;
+    border-radius: 10px;
+    overflow: hidden;
+    box-shadow: 18px 18px 48px rgb(108 99 255 / 10%);
+    transition: all .3s ease;
+  }
+
+  .main-menu ul li.active{
+    background-color: var(--color-main);
+  }
+
+  .main-menu ul li.active span:hover {
+    left: 2.875rem;
+    color: #FFF;
+  }
+
+  .main-menu ul li.active:first-child {
+    width: 130px;
+  }
+
+  .main-menu ul li.active:nth-child(2) {
+    width: 160px;
+  }
+
+  .main-menu ul li.active:nth-child(3) {
+    width: 160px;
+  }
+
+  .main-menu ul li.active:nth-child(4) {
+    width: 160px;
+  }
+
+  .main-menu ul li.active:nth-child(5) {
+    width: 160px;
+  }
+  
+
+  .main-menu ul li:hover:first-child{
+    width: 130px;
+  }
+
+  .main-menu ul li:hover:nth-child(2){
+    width: 160px;
+  }
+
+  .main-menu ul li:hover:nth-child(3){
+    width: 160px;
+  }
+
+  .main-menu ul li:hover:nth-child(4){
+    width: 160px;
+  }
+
+  .main-menu ul li:hover:nth-child(5){
+    width: 160px;
+  }
+
+  .main-menu ul li a{
+    text-decoration: none;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .main-menu ul li a{
+   justify-content: flex-start;
+   padding-left: 22px;
+  }
+
+  .main-menu ul li:hover a span{
+   color: #000;
+   left: 46px;
+  }
+
+  .main-menu ul li span{
+    font-size: 16px;
+    color: #222;
+    font-weight: 700;
+    left: 100%;
+    top: 50%;
+    -webkit-transform: translateY(-50%);
+    transform: translateY(-50%);
+    padding-left: 12px;
+    position: absolute;
+    white-space: nowrap;
+    text-transform: uppercase;
+    transition: all .3s ease;
+  }
+
+  .main-menu ul li i{
+    font-size: 25px;
+    color: #ff451b;
+    display: block;
+    transition: all .3s ease;
+  }
+
+  .main-menu ul li.active a span{
+   color: #FFFF;
+   left: 46px;
+  }
+
+  .main-menu ul li.active i{
+   color: #FFFF;
+  }
+</style>
